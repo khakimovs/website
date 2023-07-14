8b4fa9004978b594dc8feaf86b80d7d81b2c4419
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2023-07-14 21:46:42 +0500

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -12,35 +12,33 @@ export const metadata: Metadata = {
 export default function AboutPage() {
   return (
     <section>
-      <h1 className="font-bold text-3xl font-serif">About Me</h1>
-      <p className="my-5 text-neutral-800 dark:text-neutral-200">
-        Hey, I'm Lee. Most folks know me as <b>leerob</b> online.
-      </p>
+      <h1 className="font-bold text-3xl font-serif">About Us</h1>
       <div className="prose prose-neutral dark:prose-invert text-neutral-800 dark:text-neutral-200">
         <p>
-          I'm currently the <b>VP of Developer Experience at Vercel</b>, where I
-          lead our Developer Relations and Documentation teams. I focus on{' '}
-          <b>educating and growing</b> the Vercel and Next.js communities.
+          We are the Khakimov family, a lineage renowned for producing individuals
+          of influence and distinction. Our family, spanning generations, comprises
+          professionals from a diverse array of fields, creating a mosaic of
+          expertise and perspectives.
         </p>
         <hr />
         <p>
-          I'm passionate about many creative pursuits, including music,
-          photography, videography, and of course, coding. This combination of
-          interests is what ultimately led me to my current role in building
-          developer communities.
+          We are not merely a family bound by blood, but also by our shared commitment
+          to fostering innovation and advancing knowledge. Our endeavors have
+          consistently extended into wide-ranging fields of study, with a unifying
+          emphasis on contributing original research and creating novel, impactful
+          solutions.
         </p>
         <p>
-          I <b>love</b> building for the web. From something as simple as a
-          single HTML file – all the way to large Next.js applications. The web
-          is incredible. Anyone can become a developer, writer, or creator – and
-          no one has to ask for permission. You can just build.
+          Our members distinguish themselves through their unique abilities to challenge
+          conventional wisdom and deliver transformative insights in their respective
+          disciplines. As such, the Khakimov family has not only contributed to the
+          intellectual and cultural wealth of our society but has also significantly
+          shaped various academic and professional landscapes.
         </p>
         <p className="mb-8">
-          Outside of Vercel, I <b>angel invest</b> in developer tools companies
-          and <b>advise early-stage startups</b>. I also do Developer Relations
-          consulting work, helping companies take their DevRel function from 0
-          to 1, or provide guidance on growing communities, content creation,
-          and developer marketing.
+          Our mission, as a family, is to continue this legacy of innovation and
+          influence. We strive to contribute to society through our research, to pioneer
+          change, and to inspire others through our accomplishments.
         </p>
         <div className="flex flex-col gap-2 md:flex-row md:gap-2">
           <a
