9e6f55ab2e36701901ec3954bdf9d044ee246490
Sokhibjon Orzikulov <sakhib@orzklv.uz>
2023-11-15 18:36:32 +0500

    new favicon & og image

--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -24,7 +24,7 @@ export default async function handler(req: NextRequest) {
           flexDirection: "column",
           alignItems: "flex-start",
           justifyContent: "center",
-          backgroundImage: "url(https://leerob.io/og-bg.png)",
+          backgroundImage: "url(https://khakimovs.uz/og-bg.png)",
         }}
       >
         <div
